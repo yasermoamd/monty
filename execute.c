b49aed4886547c5d9a92cc45198c14ffbb42556e
yasermoamd <yasermoamd@hotmail.com>
2024-03-28 04:24:22 -0700

    updated

--- execute.c
+++ execute.c
@@ -12,12 +12,7 @@ int execute(char *line, stack_t **stack, unsigned int count)
 {
 	/*char *command, *arg;*/
 	instruction_t func[] = {
-		{"push", push}, {"pall", pall},
-		{"pint", pint}, {"pop", pop},
-		{"swap", swap}, {"add", add},
-		{"sub", sub}, {"div", _div},
-		{"mul", mul}, {"mod", mod},
-		{"nop", nop},
+		{"push", push},
 	};
 	char *command = strtok(line, " \n\t");
 	char *arg;
