6c1f5d3df9b2dcca7bf1df462689dbf27affd3aa
Malaz-22 <malazm09@gmail.com>
2024-03-28 19:28:58 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - doesn’t do anything.
+ * @stack: the head of the stack
+ * @line_number: the file's line
+ * Return: nothing
+*/
+void f_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
