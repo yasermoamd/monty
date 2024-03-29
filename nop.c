302309913467dc28e70b7e07078b2e45dc755f60
Malaz-22 <malazm09@gmail.com>
2024-03-28 19:33:41 -0700

    nop

--- nop.c
+++ nop.c
@@ -6,7 +6,7 @@
  * @line_number: the file's line
  * Return: nothing
 */
-void f_nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
